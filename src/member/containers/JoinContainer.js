2d6e5c342e9e381a2d5fc689fab5f0fe9ede9180
SongHyunmsoo <y85858@naver.com>
2024-02-28 19:05:41 +0900

    .

--- src/member/containers/JoinContainer.js
+++ src/member/containers/JoinContainer.js
@@ -1,11 +1,15 @@
 import React, { useState, useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
+import { apiJoin } from '../apis/apiJoin';
 import JoinForm from '../components/JoinForm';
+import { produce } from 'immer';
+import { useNavigate } from 'react-router-dom';
 
 const JoinContainer = () => {
   const [form, setForm] = useState({}); // 양식 항목 데이터
   const [errors, setErrors] = useState({}); // 유효성 검사 실패시 필드, 메세지
   const { t } = useTranslation();
+  const navigate = useNavigate();
 
   const onSubmit = useCallback(
     (e) => {
@@ -21,6 +25,7 @@ const JoinContainer = () => {
       /* 필수 항목 E */
 
       const _errors = {}; // 검증 실패시 담아주는 에러 객체
+      let hasErrors = false; // 에러 유무
 
       /* 필수 항목 검증 S */
       for (const [key, value] of Object.entries(requiredFields)) {
@@ -29,25 +34,59 @@ const JoinContainer = () => {
         const fieldValue =
           key === 'agree' ? form[key] : form[key] ? form[key].trim() : '';
 
-        if (!fieldValue) _errors[key].push(value);
+        if (!fieldValue) {
+          _errors[key].push(value);
+          hasErrors = true;
+        }
       }
       /* 필수 항목 검증 E */
-
       setErrors(_errors);
+
+      /* 회원가입 요청 처리 S */
+      if (!hasErrors) {
+        apiJoin(form)
+          .then(() => {
+            navigate('/member/login'); // 회원 가입 성공시 로그인 페이지 이동
+          })
+          .catch((err) => {
+            if (err.message) {
+              err.messages = err.messages || {};
+              err.messages.global = err.messages.global || [];
+              err.messages.global.push(err.message);
+            }
+
+            if (err.messages) {
+              for (const [key, values] of Object.entries(err.messages)) {
+                if (values && values.length > 0) {
+                  _errors[key] = values;
+                }
+              }
+
+              setErrors({ ..._errors });
+            }
+          });
+      }
+      /* 회원가입 요청 처리 E */
     },
-    [t, form],
+    [t, form, navigate],
   );
 
   const onChange = useCallback((e) => {
-    setForm((form) => ({ ...form, [e.target.name]: e.target.value.trim() }));
+    setForm(
+      produce((draft) => {
+        draft[e.target.name] = e.target.value.trim();
+      }),
+    );
   }, []);
 
   const onToggle = useCallback(
     () =>
-      setForm((form) => {
-        form.agree = form.agree || false;
-        return { ...form, agree: !form.agree };
-      }),
+      setForm(
+        produce((draft) => {
+          draft.agree = draft.agree || false;
+          draft.agree = !draft.agree;
+        }),
+      ),
     [],
   );
 
--- src/member/containers/LoginContainer.js
+++ src/member/containers/LoginContainer.js
@@ -2,12 +2,23 @@ import React, { useState, useCallback, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 import LoginForm from '../components/LoginForm';
 import { produce } from 'immer';
+import { apiLogin, updateMemberInfo } from '../apis/apiLogin';
+import cookies from 'react-cookies';
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import UserContext from '../modules/UserContext';
 
 const LoginContainer = () => {
+  // 폼 데이터는 함수로만 컨트롤 가능하다
   const [form, setForm] = useState({});
   const [errors, setErrors] = useState({});
 
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
+  const redirectURL = searchParams.get('redirectURL') || '/';
+
+  const userContext = useContext(UserContext);
 
   const onChange = useCallback(
     (e) =>
@@ -31,18 +42,42 @@ const LoginContainer = () => {
       /* 필수 입력 항목 E */
 
       const _errors = {};
+      let hasErrors = false; // 에러가 발생해도 보내지 않는다
 
       for (const [key, value] of Object.entries(requiredFields)) {
         _errors[key] = _errors[key] || [];
-        form[key] = form[key] || '';
-        if (!form[key].trim()) {
+        // 폼에 키가 없을떄, 폼에 trim 이 없을때 에러 발생
+        if (!form[key] || !form[key].trim()) {
           _errors[key].push(value);
+          hasErrors = true;
         }
       }
 
       setErrors(_errors);
+
+      if (hasErrors) {
+        return; // 에러가 발생시 종료된다.
+      }
+
+      apiLogin(form)
+        .then((token) => {
+          // 성공시 then 에 유입
+          cookies.save('token', token, {
+            path: '/',
+          });
+
+          updateMemberInfo(userContext); // 회원정보, 로그인 상태, 관리자 여부 업데이트
+
+          navigate(redirectURL);
+        })
+        .catch((err) => {
+          // 실패시 catch 로 유입
+          _errors.global = _errors.global || [];
+          _errors.global.push(err.messages);
+          setErrors({ ..._errors });
+        });
     },
-    [form, t],
+    [form, t, navigate, redirectURL, userContext],
   );
 
   return (
